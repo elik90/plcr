6a456a7177fbe37f86f28ca02443050253570f8e
elik90 <echoo12@gmail.com>
2020-10-07 22:58:35 -0500

    class and initial sql

--- pl_reader.py
+++ pl_reader.py
@@ -1,5 +1,7 @@
 import os.path
 import pandas as pd
+import re
+from psycopg2 import OperationalError
 # Script to open m3u8 files and pull folder name and title
 # Folder name (Artist - Album) then needs to get split into Artist / Album
 # Artist, Album, Title should all be entered into sql DB
@@ -9,12 +11,15 @@ ext = ('.mp3', '.wav', '.flac', '.aac', '.ogg')
 # READ USER INPUT SPECIFIED FILE AND ASSIGN LINES THAT END WITH AUDIO EXTENSION INTO audio_lines ARRAY
 
 class Track:
-    def __init__(self):
-        self.title = file    #strip ext and track number
-        self.folder = 
+    def __init__(self, folder, artist, album, title):
+        self.folder = folder
+        self.artist = artist
+        self.album = album
+        self.title = title    #strip ext and track number
+        print(self.artist + " - " + self.album + " - " + self.title)
 
-
-    def 
+    def __str__(self):
+            return 'aowiejfoiajwoeifjaoiwejfoiawjef{} - {} - {}'.format(self.artist, self.album, self.title)
 
 
 
@@ -26,54 +31,110 @@ with open(file_name) as input_file:
 for line in playlist_byline:
     if line.endswith(ext):
         audio_lines.append(line)
-print(audio_lines)
 
-# CREATE playlist_dict WITH KEY:VALUE AS artist/album:track 
-playlist_dict = {}
-for i in audio_lines:
-    path, file = os.path.split(i)
+#print(audio_lines)
+print(*audio_lines, sep = "\n") 
+
+playlist = []
+for line in audio_lines:
+    track_dict = {'Folder' : '', 'Artist' : '', 'Album' : '', 'Title' : ''}
+    path, title = os.path.split(line)
     path_short = path.replace('primary/Music/PowerAmpP/', '')
+#   file, playlist[i] = track_dict.update({'Title' : file})
+    track_dict['Folder'] = path_short
+    #re.split(r'[-]*',short_path)
+    regex = r"^(.*)\s-\s(.*)"
 
-    if path_short in playlist_dict:
-        playlist_dict[path_short].append(file)
+    test_str = path_short
+
+    #matches = re.finditer(regex, test_str)
+
+    # for matchNum, match in enumerate(matches):
+    #     for groupNum in range(0, len(match.groups())):
+    #         groupNum = groupNum + 1
+    #         print ("Group {groupNum}: {group}".format(groupNum = groupNum, group = match.group(groupNum)))
+    # track_dict['Artist'] = match.group(1)
+    # track_dict['Album'] = match.group(2)
+    #===========================================================================
+    matches = re.match(regex, test_str)
+    if matches != None:
+        track_dict['Artist'] = matches.group(1)
+        track_dict['Album'] = matches.group(2)
     else:
-        playlist_dict[path_short] = [file]
+        track_dict['Artist'] = "unknown"
+        track_dict['Album'] = "unknown"
+    #print("testgroup2 =" m.group(1))
+    #track_dict['Artist'] = match.group(1)
+    #track_dict['Album'] = match.group(2)
+    
+    track_dict['Title'] = title
+    playlist.append(track_dict)
+
+## Populates playlist_dict with key = folder, values = list of files
+#    if path_short in playlist_dict:
+#        playlist_dict[path_short].append(file)
+#    else:
+#        playlist_dict[path_short] = [file]
 
 
+#print(track_dict)
+print("________________________________")
+print(playlist)
 
-
-
-
+print(*playlist, sep = "\n")
 
 # 
-pd.set_option('display.max_colwidth', None)
-playlist_df = pd.DataFrame(playlist_dict.items(), columns = ["Folder", "Track"])
-
+#pd.set_option('display.max_colwidth', None)
+#playlist_df = pd.DataFrame(playlist_dict.items(), columns = ["Folder", "Track"])
 # SPLIT ARTIST FROM ALBUM
 #playlist_df[['Artist', 'Album']] = playlist_df.Folder.str.split("-", expand = True)
+#print(playlist_df)
 
-print("________________________________")
-print(playlist_df)
+# create list of objects using class Track
+tracks = []
+qty = len(playlist)
+print(qty)    #86
+for i in range(1,qty):
+    tracks.append("Track" + str(i))
+print(tracks)
+
+for idx, val in enumerate(tracks):
+    val = Track(playlist[idx]['Folder'], playlist[idx]['Artist'], playlist[idx]['Album'], playlist[idx]['Title'])
+    #print(idx, val.folder)
+
+#test = playlist[0]['Folder']
+# test = track73.getattr()
+print(type(tracks[6]))
+thetrack = Track('FOLDER', 'ARTIST', 'ALBUM', 'TITLE')
+print(getattr(thetrack, 'folder'))
+print(type(thetrack))
+
+#objs = [Track() for i in range(qty)]
 
 
-
-### Breaks up artist-album into folder and subfolders if CD1.. exists
-# folders = []      
-# print(folders)
-# while 1:
-#     path_short, folder = os.path.split(path)
-#     print(f"while loop path: " + path_short)
-#     print(f"while loop folder: " + folder)
-#     print(folders)
-#     if folder != ("" or "PowerAmpP" or "Music" or "primary"):
-#         folders.append(folder)
-#     break   
-# folders.reverse()
-# print(folders)
-# print(file)
+objs = list()
+for idx in range(qty):
+    objs.append(Track(playlist[idx]['Folder'], playlist[idx]['Artist'], playlist[idx]['Album'], playlist[idx]['Title']))
+print(objs)
 
 
+for obj in objs:
+    print(obj)
 
-    ## Add y/z.mp3 into SQL DB as y = folder, z = filename
-    ## split y into columns 'artist' and 'album' based on '-' character.  if no '-', do nothing.
 
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycop2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationError as e:
+        print(f"The error '{e}' occured")
+    return connection
+
+    
