c8adacce80c84625d38632e3fa9b910555cbd630
elik90 <echoo12@gmail.com>
2020-10-22 03:22:29 -0700

    remove sql test files

--- sql_test2.py
+++
@@ -1,7 +0,0 @@
-import pyodbc
-
-
-for driver in pyodbc.drivers():
-    print(driver)
-
-con = pyodbc.connect('Trusted_Connection=yes', driver = '{SQL Server}',server = 'localhost', database = 'dbtesttest')
