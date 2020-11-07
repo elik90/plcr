f7b7d5f00f3d2ceda71652e788b30e3f7c08787b
elik90 <echoo12@gmail.com>
2020-11-07 12:34:21 -0600

    csv fix and object attr calls

--- pl_format.py
+++ pl_format.py
@@ -1,7 +1,10 @@
 import os.path
 import re
+import csv
+#import pathlib
 
 from tracks import *
+from pathlib import Path
 
 ext = ('.mp3', '.wav', '.flac', '.aac', '.ogg')
 # create list of dictionaries
@@ -15,7 +18,7 @@ class Playlist:
         self.playlist = []
         self.objs = []
         #self.tracklist = Track.objects()
-    
+
     def create_audio_lines(self):
         # list of raw lines
         with open(self.pl_file) as input_file:
@@ -26,8 +29,9 @@ class Playlist:
         # remove header in path
         for idx, line in enumerate(self.audio_lines):
             self.audio_lines[idx] = line.replace('primary/Music/PowerAmpP/', '')
+            self.audio_lines[idx] = os.path.splitext(self.audio_lines[idx])[0]
         return 
-        
+
 
     def create_playlist(self):
         # list of dictionaries of track details
@@ -43,7 +47,7 @@ class Playlist:
                 track_dict['artist'] = matches.group(1)
                 track_dict['album'] = matches.group(2)
             else:
-                print("{}*****FIX THIS*****".format(track_dict['folder']))
+                #print("{}*****FIX THIS*****".format(track_dict['folder']))
                 track_dict['artist'] = "unknown"
                 track_dict['album'] = "unknown"
             self.playlist.append(track_dict)
@@ -53,14 +57,18 @@ class Playlist:
         qty = len(self.playlist)
         for int in range(qty):
             self.objs.append(Track(self.playlist[int]['folder'], self.playlist[int]['artist'], self.playlist[int]['album'], self.playlist[int]['title']))
+
+    def list_tracks(self):
+        qty = len(self.playlist)
+        print(qty)
+        for int in range(qty):
             print("{}. {} ".format(int, self.objs[int]))
 
-    # def save(self):
-    #     my_path = os.path.abspath(os.path.dirname(__file__))
-    #     path = os.path.join(my_path, "../data/playlist.csv")
+    def save(self):
+        csv_name = "{}.csv".format(self.name)
+        path = Path.cwd() / 'data' / csv_name
 
-    #     with open(path, 'w') as csvfile:
-    #         playlist_csv = csv.writer(csvfile, delimiter=',')
-    #         playlist_csv.writerow(['name', 'age', 'role', 'school_id', 'password'])
-    #         for track in self.tracklist:
-    #             playlist_csv.writerow([track.folder, track.artist, track.album, track.title])
+        with open(path.absolute(), mode='w') as csv_file:
+            playlist_csv = csv.writer(csv_file, delimiter=',')
+            for v in self.objs:
+                playlist_csv.writerow([v.artist, v.album, v.title])
--- pl_reader.py
+++ pl_reader.py
@@ -1,10 +1,8 @@
-import psycopg2
-import csv
 import pandas as pd
 
-from psycopg2 import OperationalError
 from spotify_client import *
 from pl_format import *
+#from dbcontrol.py import *
 
 # Script to open m3u8 files and organize by folder, artist, album, title
 # Built using Python 3.8 & PostgreSQL 13
@@ -16,109 +14,33 @@ from pl_format import *
 # - strip ext and track number
 
 # Accept user input m3u8 file and list strings with audio file extentions in audio_lines list
-file_name = input("Enter the full file path for the m3u8 file you wish to create an SQL DB:  \n> ")
 
+
+### user supplies directory of playlist files.  menu lists options for user to choose.
+file_name = input("Enter the full file path for the m3u8 file you wish to create an SQL DB:  \n> ")
 pl = Playlist(file_name)
-print(pl.name)
 
 pl.create_audio_lines()
 pl.cleanup_path()
-print(*pl.audio_lines, sep = '\n')
+#print(*pl.audio_lines, sep = '\n')
 
 pl.create_playlist()
-print(*pl.playlist, sep = '\n')
+#print(*pl.playlist, sep = '\n')
 
 pl.create_tracks()
-print(pl.objs)
 
-# SQL Implementation
-def create_connection(db_name, db_user, db_password, db_host, db_port):
-    connection = None
-    try:
-        connection = psycopg2.connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            host=db_host,
-            port=db_port,
-        )
-        print("Connection to PostgreSQL DB successful")
-    except OperationalError as e:
-        print(f"The error '{e}' occured")
-    return connection
+#pl.list_tracks()
 
-# Create new db in postgresql db server
-def create_database(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Query executed successfully")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
+# for v in enumerate(pl.objs):
+#     print(pl.objs[v]['album'])
 
-def execute_query(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Query executed successfully")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
+# print(pl.objs[1].album)
+# print(pl.playlist[0]['artist'])
 
-def execute_db_check(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        list_database = cursor.fetchall()
-        print(list_database)
-        if 'playlists' in list_database:
-            print("'{}' Database already exists".format("playlists"))
-            return True
-        else:
-            print("'{}' Database does not exist.".format("playlists"))
-            return False
-            
+pl.save()
 
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
+# for k, v in enumerate(pl.objs):
+#     print(v.artist, v.album, v.title)
 
-
-# Connect to default database
-connection = create_connection(
-    "postgres", "postgres", "iguana90", "127.0.0.1", "5432"
-)
-db_check_query = ("SELECT datname FROM pg_database;")
-execute_db_check(connection, db_check_query)
-
-if execute_db_check is True:
-    create_database_query = "CREATE DATABASE playlists"
-    create_database(connection, create_database_query)
-else:
-
-#if db_check_query is True:
-
-    print('Done')
-
-
-# Connect to playlist database based on create_database_query above
-connection = create_connection("playlists", "postgres", "iguana90", "127.0.0.1", "5432")
-
-create_playlist_table = """
-CREATE TABLE IF NOT EXISTS Tracks (
-  Folder character (90),
-  Artist character (90), 
-  Album character (90),
-  Title character (90)
-)
-"""
-execute_query(connection, create_playlist_table)
-
-for trackdict in playlist:
-    columns = ', '.join(str(x).replace('/','_').replace('\'','') for x in trackdict.keys())
-    values = ', '.join("'" + str(x).replace('/', '_').replace('\'','') + "'" for x in trackdict.values())
-    sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % ('Tracks', columns, values)
-    connection.autocommit = True
-    cursor = connection.cursor()
-    cursor.execute(sql)
+# for k,v in enumerate(pl.playlist):
+#     print(pl.playlist[k]["artist"])
