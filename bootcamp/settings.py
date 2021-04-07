c0986c9baa61773c43e5c5c09e28316dc5225e84
elik90 <echoo12@gmail.com>
2021-04-07 17:21:40 -0500

    Update settings.py

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qa=npf5x=qzdi6n439vg^8^yl)--qe9ab9x)9-8gk*7cb7wab*'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
